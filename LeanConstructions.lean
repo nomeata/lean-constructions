bafaa8a7f03863f887472cc74d143597bb5cdb54
Joachim Breitner <mail@joachim-breitner.de>
2024-06-18 19:02:17 +0200

    Stash

--- LeanConstructions.lean
+++ LeanConstructions.lean
@@ -1,56 +1,17 @@
 import Lean
 
-
 open Lean Meta
 
-/-
-    constant_info ind_info = env.get(n);
-    if (!ind_info.is_inductive())
-        throw exception(sstream() << "error in '" << g_rec_on << "' generation, '" << n << "' is not an inductive datatype");
-    name_generator ngen = mk_constructions_name_generator();
-    local_ctx lctx;
-    name rec_on_name(n, g_rec_on);
-    constant_info rec_info = env.get(mk_rec_name(n));
-    recursor_val  rec_val  = rec_info.to_recursor_val();
-    buffer<expr> locals;
-    expr rec_type = rec_info.get_type();
-    while (is_pi(rec_type)) {
-        expr local = lctx.mk_local_decl(ngen, binding_name(rec_type), binding_domain(rec_type), binding_info(rec_type));
-        rec_type   = instantiate(binding_body(rec_type), local);
-        locals.push_back(local);
-    }
-
-    // locals order
-    //   As Cs minor_premises indices major-premise
-
-    // new_locals order
-    //   As Cs indices major-premise minor-premises
-    buffer<expr> new_locals;
-    unsigned num_indices  = rec_val.get_nindices();
-    unsigned num_minors   = rec_val.get_nminors();
-    unsigned AC_sz        = locals.size() - num_minors - num_indices - 1;
-    for (unsigned i = 0; i < AC_sz; i++)
-        new_locals.push_back(locals[i]);
-    for (unsigned i = 0; i < num_indices + 1; i++)
-        new_locals.push_back(locals[AC_sz + num_minors + i]);
-    for (unsigned i = 0; i < num_minors; i++)
-        new_locals.push_back(locals[AC_sz + i]);
-    expr rec_on_type = lctx.mk_pi(new_locals, rec_type);
-
-    levels ls = lparams_to_levels(rec_info.get_lparams());
-    expr rec  = mk_constant(rec_info.get_name(), ls);
-    expr rec_on_val = lctx.mk_lambda(new_locals, mk_app(rec, locals));
-
-    environment new_env = env.add(mk_definition_inferring_unsafe(env, rec_on_name, rec_info.get_lparams(),
-                                                                 rec_on_type, rec_on_val, reducibility_hints::mk_abbreviation()));
-    new_env = set_reducible(new_env, rec_on_name, reducible_status::Reducible, true);
-    new_env = add_aux_recursor(new_env, rec_on_name);
-    return add_protected(new_env, rec_on_name);
-
--/
+def mkDefinitionValInferrringUnsafe (name : Name) (levelParams : List Name) (type : Expr)
+    (value : Expr) : MetaM DefinitionVal := do
+  let env ← getEnv
+  return {
+    name, levelParams, type, value,
+    hints := ReducibilityHints.abbrev
+    safety := if env.hasUnsafe type || env.hasUnsafe value then DefinitionSafety.unsafe else DefinitionSafety.safe
+  }
 
 def mkRecOnValDefinitionVal (n : Name) : MetaM DefinitionVal := do
-  let env ← getEnv
   let .recInfo recInfo ← getConstInfo (mkRecName n)
     | throwError "{mkRecName n} not a recinfo"
   forallTelescope recInfo.type fun xs t => do
@@ -66,11 +27,127 @@ def mkRecOnValDefinitionVal (n : Name) : MetaM DefinitionVal := do
       xs[AC_size:AC_size + recInfo.numMinors]
     let type ← mkForallFVars vs t
     let value ← mkLambdaFVars vs e
-    return {
-      name        := mkRecOnName n
-      levelParams := recInfo.levelParams
-      type        := type
-      value       := value
-      hints       := ReducibilityHints.abbrev
-      safety      := if env.hasUnsafe type || env.hasUnsafe value then DefinitionSafety.unsafe else DefinitionSafety.safe
-    }
+    mkDefinitionValInferrringUnsafe (mkRecOnName n) recInfo.levelParams type value
+
+def mkPProd (prop : Bool) (e1 e2 : Expr) : MetaM Expr :=
+  if prop then
+    mkAppM ``And #[e1, e2]
+  else
+    mkAppM ``PProd #[e1, e2]
+
+def mkNProd (lvl : Level) (es : Array Expr) : MetaM Expr :=
+  if let .zero := lvl then
+    es.foldrM (init := .const ``True []) (mkPProd true)
+  else
+    es.foldrM (init := .const ``PUnit [lvl]) (mkPProd false)
+
+def withModifyFVarCodomain (e : Expr) (fvar : Expr) (k : MetaM α) : MetaM α := do
+  let type ← forallTelescope (← inferType fvar) fun args _ => mkForallFVars args e
+  let lctx := (← getLCtx).modifyLocalDecl fvar.fvarId! fun decl => decl.setType type
+  withReader (fun ctx => { ctx with lctx := lctx }) k
+
+def withModifyFVarsCodomain (e : Expr) (fvars : Array Expr) (k : MetaM α) : MetaM α :=
+  fvars.foldr (init := k) fun fvar k => withModifyFVarCodomain e fvar k
+
+def buildMinorPremise (rlvl : Level) (typeFormers : Array Expr) (args : Array Expr) : MetaM Expr :=
+  go 0 #[]
+where
+  ibelow := rlvl matches .zero
+  -- e0 := if ibelow then .const ``True [] else .const ``PUnit [rlvl]
+  go (i : Nat) (prods : Array Expr) : MetaM Expr := do
+    if h : i < args.size then
+      let arg := args[i]
+      let argType ← inferType arg
+      forallTelescope argType fun arg_args arg_type => do
+        if typeFormers.contains arg_type.getAppFn then
+          withModifyFVarCodomain (.sort rlvl) arg do
+            let snd ← mkForallFVars arg_args (mkAppN arg arg_args)
+            let e' ← mkPProd ibelow argType snd
+            go (i + 1) (prods.push e')
+        else
+          go (i + 1) prods
+    else
+      mkLambdaFVars args (← mkNProd rlvl prods)
+
+
+def mkBelowOrIBelow (indName : Name) (ibelow : Bool) : MetaM DefinitionVal := do
+  let indVal ← getConstInfoInduct indName
+  let recName := mkRecName indName
+  let .recInfo recVal ← getConstInfo recName
+    | throwError "{recName} not a recinfo"
+  let lvl::lvls := recVal.levelParams.map (Level.param ·)
+    | throwError "recursor {recName} has no levelParams"
+  let lvlParam := recVal.levelParams.head!
+  let blvls := -- universe parameter names of ibelow/below
+    if ibelow then recVal.levelParams.tail!
+              else recVal.levelParams
+  let rlvl : Level :=  -- universe level of the resultant type
+    if ibelow then 0
+    -- TODO: reflexive
+              else .max 1 lvl
+
+  let refType :=
+    if ibelow then
+      recVal.type.instantiateLevelParams [lvlParam] [0]
+    else if indVal.isReflexive then
+      recVal.type.instantiateLevelParams [lvlParam] [lvl.succ]
+    else
+      recVal.type
+  forallTelescope refType fun refArgs _ => do
+    assert! refArgs.size == indVal.numParams + recVal.numMotives + recVal.numMinors + indVal.numIndices + 1
+    let params      : Array Expr := refArgs[:indVal.numParams]
+    let typeFormers : Array Expr := refArgs[indVal.numParams:indVal.numParams + recVal.numMotives]
+    let minors      : Array Expr := refArgs[indVal.numParams + recVal.numMotives:indVal.numParams + recVal.numMotives + recVal.numMinors]
+    let remaining   : Array Expr := refArgs[indVal.numParams + recVal.numMotives + recVal.numMinors:]
+    let args := params ++ typeFormers ++ remaining
+
+    let mut val := .const recName (rlvl.succ :: lvls)
+    -- add parameters
+    val := mkAppN val params
+    -- add type formers
+    for typeFormer in typeFormers do
+      let arg ← forallTelescope (← inferType typeFormer) fun targs _ =>
+        mkLambdaFVars targs (.sort rlvl)
+      val := .app val arg
+    -- add minor premises
+    for minor in minors do
+      let arg ← forallTelescope (← inferType minor) fun minor_args _ => do
+        buildMinorPremise rlvl typeFormers minor_args
+      val := .app val arg
+    -- add indices and major premise
+    val := mkAppN val remaining
+    let name := if ibelow then .str indName "ibelow" else mkBelowName indName
+    let type ← mkForallFVars args (.sort rlvl)
+    val ← mkLambdaFVars args val
+    mkDefinitionValInferrringUnsafe name blvls type val
+
+deriving instance BEq for ReducibilityHints
+deriving instance BEq for DefinitionVal
+deriving instance BEq for InductiveVal
+
+def checkDefnVal (n : Name) (suffix : String) (f : Name → MetaM DefinitionVal) : MetaM Unit := do
+  let n' := .str n suffix
+  if (← hasConst n') then
+    if let .defnInfo oldVal ← getConstInfo n' then
+      let newVal ← f n
+      -- ignore inductives, to not get confused by the `.below` generated for inductive predicates
+      unless newVal == oldVal do
+        unless newVal.type == oldVal.type do
+          throwError m!"Mismatch for type of {n'}:{indentExpr oldVal.type}\nvs{indentExpr newVal.type}"
+        unless newVal.value == oldVal.value do
+          throwError m!"Mismatch for value of {n'}:{indentExpr oldVal.value}\nvs{indentExpr newVal.value}"
+        throwError m!"Mismatch for {n'}" -- ":{oldVal}\nvs{newRecOnVal}"
+
+def checkInd (n : Name) : MetaM Unit := do
+  let ci ← getConstInfo n
+  unless ci.isInductive do throwError m!"Not an inductive: {n}"
+
+  checkDefnVal n "recOn" mkRecOnValDefinitionVal
+  checkDefnVal n "below" (mkBelowOrIBelow · false)
+  -- checkDefnVal n "ibelow" mkRecOnValDefinitionVal
+
+-- #print Nat.below
+run_meta checkInd ``Acc
+
+set_option pp.explicit true in
+run_meta checkInd ``Nat
--- LeanConstructions/MathlibTest.lean
+++ LeanConstructions/MathlibTest.lean
@@ -3,25 +3,12 @@ import Mathlib
 
 open Lean Meta
 
-deriving instance BEq for ReducibilityHints
-deriving instance BEq for DefinitionVal
-
 run_meta do
   let env ← getEnv
   let mut count := 0
   for (n, ci) in env.constants do
     if ci.isInductive then
-      let recOnName := mkRecOnName n
-      unless env.contains  recOnName do
-        -- happens on .below and ._impl inductives. how to filter them?
-        -- logInfo m!"Missing {recOnName}"
-        continue
-
-      let newRecOnVal ← mkRecOnValDefinitionVal n
-      let oldVal ← getConstInfoDefn recOnName
-      unless newRecOnVal == oldVal  do
-        throwError m!"Mismatch for {n}" -- ":{oldVal}\nvs{newRecOnVal}"
+      -- IO.eprintln s!"Checking {n}"
+      checkInd n
       count := count+1
   logInfo m!"Checked {count} definitions"
-
-  return ()
